d52ac18d0e7d8824b97720c70e995a364a7cb2df
Limitex <76650151+Limitex@users.noreply.github.com>
2022-10-29 01:50:24 +0900

    Automate Python installation
    Python is installed in the Naifu directory so that it can be run whether it is already installed or not.

--- nais.bat
+++ nais.bat
@@ -1,105 +1,115 @@
 @echo off
-setlocal enabledelayedexpansion
+setlocal
 
-echo [36mNAIFU with NovelAI[0m
-echo Batch file to set it up automatically.
+set ARIA2_URI=https://github.com/aria2/aria2/releases/download/release-1.36.0/aria2-1.36.0-win-64bit-build1.zip
+set PYTHON_URI=https://www.python.org/ftp/python/3.10.8/python-3.10.8-embed-amd64.zip
+set PYTHIN_PIP_URI=https://bootstrap.pypa.io/get-pip.py
+set NAIFU_MAGNET="magnet:?xt=urn:btih:4a4b483d4a5840b6e1fee6b0ca1582c979434e4d&dn=naifu"
+
+echo :
+echo : [[36mNAIFU with NovelAI[0m]
+echo : Batch file to set it up automatically.
 echo :
 
-rem # Checking status step.
-echo Checking PC status...
+@rem # Checking status step.
 
-rem ## Check for files that exist.
-for /f "usebackq delims=" %%A in (`powershell "(Get-ChildItem -Path %CD% | Measure-Object).Count"`) do set fileCount=%%A
+echo : Checking PC status...
+
+@rem ## Check for files that exist.
+for /f "usebackq delims=" %%A in (`powershell "(Get-ChildItem -Path %~dp0 | Measure-Object).Count"`) do set fileCount=%%A
 if not %fileCount% == 1 (
-    echo [31mOther files found. Do not place any other files in the directory where this batch file is running.[0m
-    echo Example directory structure
-    echo : Directory
-    echo :  ^|-[This batch file]
-    echo :
+    echo : [31mOther files found. Do not place any other files in the directory where this batch file is running.[0m
+    echo :  Example directory structure
+    echo :   Directory
+    echo :    ^|-[This batch file]
+    echo : 
     goto :EXIT
 )
-echo The directory is empty.
+echo : The directory is empty.
 
-rem ## Check for the existence of Microsoft Visual C++ Runtime.
+@rem ## Check for the existence of Microsoft Visual C++ Runtime.
 set /a count = 0
 for /f "usebackq delims=" %%A in (`powershell -Command "Get-ChildItem -Path( 'HKLM:SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall', 'HKLM:SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall', 'HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall') | %% { Get-ItemProperty $_.PsPath | Select-Object DisplayName} | findstr /r /c:'Microsoft Visual C++.*X64.*Runtime.*' /c:'Microsoft Visual C++.*Redistributable (x64).*'"`) do (
     echo %%A | findstr "Debug Minimum Additional Redistributable" > nul
     if not errorlevel 1 set /a count += 1
 )
 if %count% equ 0 (
-    echo [33mCould not find Microsoft Visual C++ X64 Runtime.
-    echo Software may not run properly.
-    echo [34mhttps://learn.microsoft.com/en-us/cpp/windows/latest-supported-vc-redist?view=msvc-170
-    echo [33mPress any key to continue.
-    echo Press Ctrl + c to exit.[0m
+    echo : [33mCould not find Microsoft Visual C++ X64 Runtime.[0m
+    echo : [33mSoftware may not run properly.[0m
+    echo : [34mhttps://learn.microsoft.com/en-us/cpp/windows/latest-supported-vc-redist?view=msvc-170[0m
+    echo : [33mPress any key to continue.[0m
+    echo : [33mPress Ctrl + c to exit.[0m
     pause > nul
 ) else (
-    echo Microsoft Visual C++ Runtime confirmed.
+    echo : Microsoft Visual C++ Runtime confirmed.
 )
 
-rem ## Check for the existence of Python + pip.
-where python 2> nul > nul || (
-    echo [31mCould not find Python.
-    echo Install Python version 3.10 or higher.
-    echo [34mhttps://www.python.org/downloads/[0m
-    goto :EXIT
-)
-where pip 2> nul > nul || (
-    echo [31mCould not find pip.
-    echo [34mhttps://www.python.org/downloads/[0m
-    goto :EXIT
-)
-echo Python + pip was confirmed.
-echo An error may occur if the version is not 3.10 or higher.
-echo This message is printed even on 3.10 and above.
 echo :
-echo [32mConfirmation completed.[0m
+echo : [32mConfirmation completed.[0m
 
-rem # Build step.
+@rem # Build step.
+
 echo :
-echo [33mThe process may take some time or may not be completed because of the Torrent downloading process during the process.[0m
+echo : Start the build step
 echo :
-echo Start build.
 
-echo [36mUpdating pip...[0m
-python -m pip install --upgrade pip
+@rem ## Download the aria2c tool
+echo : Preparing the download tool.
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%ARIA2_URI%','%~dp0aria2.zip')"
+powershell Expand-Archive -Path "%~dp0aria2.zip" -DestinationPath "%~dp0aria2" > nul
+del "%~dp0aria2.zip"
+for /f "usebackq delims=" %%A in (`where /r "%~dp0aria2" *.exe`) do set ARIAPATH=%%A
+move %ARIAPATH% %~dp0 > nul
+rd /s /q "%~dp0aria2"
 
-echo [36mInstalling required Python packages...[0m
-pip install torch torchvision torchaudio dotmap fastapi uvicorn omegaconf transformers sentence_transformers faiss-cpu einops pytorch_lightning ftfy scikit-image torchdiffeq jsonmerge --extra-index-url https://download.pytorch.org/whl/cu116
+@rem ## Download the NAIFU
+echo : Download the Naifu.
+%~dp0aria2c.exe --seed-time=0 %NAIFU_MAGNET%
+del "%~dp0aria2c.exe"
 
-echo [36mPreparing aria2...[0m
-powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://github.com/aria2/aria2/releases/download/release-1.36.0/aria2-1.36.0-win-64bit-build1.zip','%CD%\aria2.zip')"
-powershell Expand-Archive -Path "%CD%\aria2.zip" -DestinationPath "%CD%\aria2" > nul
-del "%CD%\aria2.zip"
-for /f "usebackq delims=" %%A in (`where /r "%CD%\aria2" *.exe`) do set ARIAPATH=%%A
-move %ARIAPATH% %CD% > nul
-rd /s /q "%CD%\aria2"
+@rem ## Download Python
+echo : Download the Python.
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%PYTHON_URI%','%~dp0python.zip')"
+powershell Expand-Archive -Path "%~dp0python.zip" -DestinationPath "%~dp0naifu\python" > nul
+del "%~dp0python.zip"
 
-echo [36mDownload NAIFU...[0m
-%CD%\aria2c.exe --seed-time=0 "magnet:?xt=urn:btih:4a4b483d4a5840b6e1fee6b0ca1582c979434e4d&dn=naifu"
-del "%CD%\aria2c.exe"
+@rem ## Preparing PIP
+echo : Preparing for pip.
+powershell -Command "(New-Object System.Net.WebClient).DownloadFile('%PYTHIN_PIP_URI%','%~dp0get-pip.py')"
+%~dp0naifu\python\python.exe %~dp0get-pip.py > nul
+del %~dp0get-pip.py
+powershell -Command "[System.IO.File]::WriteAllLines(('%~dp0replaced'), @((gc '%~dp0naifu\python\python310._pth').Replace('#import site', 'import site')), (New-Object 'System.Text.UTF8Encoding' -ArgumentList @($false)))" > nul
+copy /y %~dp0replaced %~dp0naifu\python\python310._pth
+del /q %~dp0replaced
 
-echo [36mSetup...[0m
-powershell Expand-Archive -Path "%CD%\naifu\program.zip" -DestinationPath "%CD%\naifu" > nul
-del "%CD%\naifu\program.zip"
+@rem ## Install Python modules
+echo : Installs the modules required to run Naifu.
+%~dp0naifu\python\Scripts\pip.exe install torch torchvision torchaudio dotmap fastapi uvicorn omegaconf transformers sentence_transformers faiss-cpu einops pytorch_lightning ftfy scikit-image torchdiffeq jsonmerge --extra-index-url https://download.pytorch.org/whl/cu116
 
-powershell -Command "[System.IO.File]::WriteAllLines(('%CD%\naifu\replaced.txt'), @((gc '%CD%\naifu\run.bat').Replace('%%~dp0%%VENV_DIR%%\Scripts\Python.exe', 'python').Replace('--host 0.0.0.0', '--host 127.0.0.1').Replace('--port=6969', '--port=80')), (New-Object 'System.Text.UTF8Encoding' -ArgumentList @($false)))" > nul
-copy /y %CD%\naifu\replaced.txt %CD%\naifu\run.bat
-del /q %CD%\naifu\replaced.txt
+@rem ## Compose the naifu
+echo : Setting up Naifu.
+powershell Expand-Archive -Path "%~dp0naifu\program.zip" -DestinationPath "%~dp0naifu" > nul
+del "%~dp0naifu\program.zip"
 
-mkdir %CD%\naifu\others > nul
-move %CD%\naifu\frontend-src.zip %CD%\naifu\others > nul
-move %CD%\naifu\requirements.txt %CD%\naifu\others > nul
-move %CD%\naifu\README.txt %CD%\naifu\others > nul
-move %CD%\naifu\setup.bat %CD%\naifu\others > nul
-move %CD%\naifu\setup.sh %CD%\naifu\others > nul
-move %CD%\naifu\run.sh %CD%\naifu\others > nul
+powershell -Command "[System.IO.File]::WriteAllLines(('%~dp0replaced'), @((gc '%~dp0naifu\run.bat').Replace('%%~dp0%%VENV_DIR%%\Scripts\Python.exe', 'python\python.exe').Replace('--host 0.0.0.0', '--host 127.0.0.1').Replace('--port=6969', '--port=80')), (New-Object 'System.Text.UTF8Encoding' -ArgumentList @($false)))" > nul
+copy /y %~dp0replaced %~dp0naifu\run.bat
+del /q %~dp0replaced
 
-copy %CD%\naifu\static\icons\novelai-round.png %CD%\naifu\static\icons\novelai512.png > nul
+mkdir %~dp0naifu\others > nul
+move %~dp0naifu\frontend-src.zip %~dp0naifu\others > nul
+move %~dp0naifu\requirements.txt %~dp0naifu\others > nul
+move %~dp0naifu\README.txt %~dp0naifu\others > nul
+move %~dp0naifu\setup.bat %~dp0naifu\others > nul
+move %~dp0naifu\setup.sh %~dp0naifu\others > nul
+move %~dp0naifu\run.sh %~dp0naifu\others > nul
+copy %~dp0naifu\static\icons\novelai-round.png %~dp0naifu\static\icons\novelai512.png > nul
 
-echo [32mAll operations are complete.
-echo You can run it from the run.bat file in naifu.[0m
-echo ^(Done^)
+echo : 
+echo : [32mAll operations are complete.[0m
+echo : [32mYou can run it from the run.bat file in naifu.[0m
+echo :
+echo : ^(Done^)
+echo :
 
 :EXIT
 pause
